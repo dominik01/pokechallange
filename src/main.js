d8662efb938eccf230045c6868a535af8563a382
Dominik Knechta <dominik.knechta@eset.sk>
2021-08-11 19:38:04 +0200

    Removed global use

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export const pokeApiUrl = 'https://pokeapi.co/api/v2'
 
 Vue.config.productionTip = false
-Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
 new Vue({
